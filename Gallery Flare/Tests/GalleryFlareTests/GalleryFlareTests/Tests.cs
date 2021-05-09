729b3f3bab6105245bccf8e23db066eb44d337ea
Jawad Jawid <jawadadham98@gmail.com>
2021-05-09 00:02:03 -0400

    update tests url to public server

--- Gallery Flare/Tests/GalleryFlareTests/GalleryFlareTests/Tests.cs
+++ Gallery Flare/Tests/GalleryFlareTests/GalleryFlareTests/Tests.cs
@@ -14,7 +14,7 @@ namespace GalleryFlareTests
 {
     public class Tests
     {
-        public string _baseUri = "https://localhost:44339";
+        public string _baseUri = "https://galleryflare.azurewebsites.net";
 
         private static Random random = new Random();
         public static string RandomString(int length)
